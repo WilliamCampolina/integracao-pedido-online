69daaf311c0908e6745b8b8623f34a5ca856615e
William <william.campolina@gmail.com>
2019-04-01 14:30:39 -0300

    token funcionando

--- geraToken.py
+++ geraToken.py
@@ -3,6 +3,8 @@ import datetime
 import hashlib
 import base64
 import requests
+import json
+from decouple import config
 
 """
 Classe para integração com sistema MisterChef
@@ -11,63 +13,48 @@ http://tdn.totvs.com/display/public/TChef/API+Pedidos+Online#APIPedidosOnline-Au
 
 """
 
-HOST = 'http://hchefweb.bematech.com.br/IntegracaoPedidosOnline/'
+HOST = 'http://chefweb.bematech.com.br/IntegracaoPedidosOnline/'
 HHOST = 'http://hchefweb.bematech.com.br/IntegracaoPedidosOnline/'
 
 class GenerateToken:
 
-    def __init__(self, codigo_integracao, chave_cliente):
+    def __init__(self, codigo_integracao, chave_cliente, codigo_estabelecimento):
+        self.codigo_estabelecimento = codigo_estabelecimento
         self.codigo_integracao = codigo_integracao
         self.chave_cliente = chave_cliente
 
     def gerarToken(self):
         data = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
         token = str(self.codigo_integracao) +"|"+ str(self.chave_cliente) +"|"+ data
+        print(token)
         token_md5 = hashlib.md5(token.encode('utf-8')).hexdigest()
+        token_md5 = data+":"+token_md5
+        print(token_md5)
         token_base64 = base64.b64encode(token_md5.encode('UTF-8')).decode('ascii')
         return 'Basic ' + token_base64
 
 
-    def send_test(self, token):
+    def sendTest(self, token):
 
-        url = HHOST + "CadastroService.svc/ObterCardapio"
-        datas = {
-                   "parametros": {
-                      "CodigoEstabelecimento": "96700001",
-                      "CodigoIntegracao": 1,
-                      "DataAtualizacao": "null"
-                   }
-                }
+        url = HOST + "CadastroService.svc/ObterCardapio"
+        datas = "{\n   \"parametros\": {\n      \"CodigoEstabelecimento\": \""+self.codigo_estabelecimento +"\",\n      \"CodigoIntegracao\": 1\n   }\n}"
 
-        headers = {'Content-type': 'application/json',
-                   'Authorization': token
-                   }
+        headers = {
+            'authorization': token,
+            'content-type': "application/json",
+            'cache-control': "no-cache",
+        }
 
-        rsp = requests.post(url, json=datas, headers=headers)
-        dump(rsp)
+        rsp = requests.post(url, data=datas, headers=headers)
+        print(rsp.json())
 
-
-def dump(obj):
-   for attr in dir(obj):
-       if hasattr( obj, attr ):
-           print( "obj.%s = %s" % (attr, getattr(obj, attr)))
 
 if __name__ == '__main__':
 
-    gerador = GenerateToken(codigo_integracao=1,chave_cliente=96725436)
+    gerador = GenerateToken(codigo_integracao=1,chave_cliente=config('KEY'), codigo_estabelecimento=config('ESTABELECIMENTO'))
     token = gerador.gerarToken()
     print(token)
 
-    gerador.send_test(token)
-
-    #19672543620190329160555
-
-    #df15bb9b5f5f3178c915da34092205f5
-    #DF15BB9B5F5F3178C915DA34092205F5
-
-    #ZGYxNWJiOWI1ZjVmMzE3OGM5MTVkYTM0MDkyMjA1ZjU=
-    #REYxNUJCOUI1RjVGMzE3OEM5MTVEQTM0MDkyMjA1RjU=
-
-
+    gerador.sendTest(token)
 
 
